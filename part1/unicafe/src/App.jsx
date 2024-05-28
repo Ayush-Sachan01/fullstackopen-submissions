5a4ee36e23a238d836975bfb202ce25854978dc0
Ayush <ayushsachan49@gmail.com>
2024-05-28 21:19:29 +0530

    Unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,19 @@
 import { useState } from 'react'
 
+const Statistics = ({good, neutral, bad}) => {
+  return(
+  <div>
+    <h1>statistics</h1> 
+      good {good} <br />
+      neutral {neutral} <br />
+      bad {bad} <br />
+      all {good+neutral+bad} <br />
+      average {(good+bad*(-1))/(good+neutral+bad)} <br />
+      positive {good/(good+neutral+bad)*100} % <br />
+  </div>
+  )
+  }
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -12,13 +26,7 @@ const App = () => {
       <button onClick={()=>setGood(good+1)}>good</button>
       <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
       <button onClick={()=>setBad(bad+1)}>bad</button>  
-      <h1>statistics</h1>
-      good {good} <br />
-      neutral {neutral} <br />
-      bad {bad} <br />
-      all {good+neutral+bad} <br />
-      average {(good+bad*(-1))/(good+neutral+bad)} <br />
-      positive {good/(good+neutral+bad)*100} % <br />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
