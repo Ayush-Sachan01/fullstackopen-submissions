9e6d8c9183cf30ba167061ca4366ebf470ebacf3
Ayush <ayushsachan49@gmail.com>
2024-05-28 20:53:39 +0530

    Unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -15,7 +15,10 @@ const App = () => {
       <h1>statistics</h1>
       good {good} <br />
       neutral {neutral} <br />
-      bad {bad}
+      bad {bad} <br />
+      all {good+neutral+bad} <br />
+      average {(good+bad*(-1))/(good+neutral+bad)} <br />
+      positive {good/(good+neutral+bad)*100} % <br />
     </div>
   )
 }
