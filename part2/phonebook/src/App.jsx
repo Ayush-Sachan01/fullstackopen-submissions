229ba28c798cb4bf06e40e42b269a0805008caff
Ayush <ayushsachan49@gmail.com>
2024-05-30 16:50:31 +0530

    phonebook step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-123456'
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -27,7 +28,8 @@ const App = () => {
     setNewName('')
     setNewNumber('')
   }
-
+   
+  const filtered = persons.filter(person=>person.name.toLowerCase().includes(filter.toLowerCase())).map(person=><p key={person.name}>{person.name} {person.number}</p>)
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
@@ -39,15 +41,30 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+
+    console.log(event.target.value)
+    setFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input 
+        value={filter}
+        onChange={handleFilterChange}
+        />
+             
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input
             value={newName}
             onChange={handleNameChange}
           />
+          
         </div>
         <div>number: <input
           value={newNumber}
@@ -58,7 +75,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      {/* {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)} */}
+      {filtered}
     </div>
   )
 }
