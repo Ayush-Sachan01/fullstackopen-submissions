35f88a91fae081e118760f141b82f784fc0539d4
Ayush <ayushsachan49@gmail.com>
2024-05-31 16:52:30 +0530

    phonebook step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-
+import { useState,useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ text, value, onChange }) => {
   return (
@@ -43,16 +43,18 @@ const Person = ({ person }) => {
 const App = () => {
 
   // User states
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
 
+    useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
  // event handlers
   const addName = (event) => {
