19dced927f672977ec7f0a7a60b443319aabea01
Ayush <ayushsachan49@gmail.com>
2024-06-01 11:03:15 +0530

    phonebook part 8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState,useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
+
+
 
 const Filter = ({ text, value, onChange }) => {
   return (
@@ -38,8 +40,6 @@ const Person = ({ person }) => {
   )
 }
 
-
-
 const App = () => {
 
   // User states
@@ -49,10 +49,8 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
     useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService.getAll().then(initialNote => {
+        setPersons(initialNote)
       })
   }, [])
 
@@ -70,11 +68,12 @@ const App = () => {
     }
     else {
       // setPersons(persons.concat(personObject))
-     axios
-       .post('http://localhost:3001/persons', personObject)
-       .then(response=>{
-        setPersons(persons.concat(response.data))
-       })     
+     personService
+      .create(personObject)
+      .then(returnedNote=>{
+        setPersons(persons.concat(returnedNote))
+      
+      })    
     }
  
 
