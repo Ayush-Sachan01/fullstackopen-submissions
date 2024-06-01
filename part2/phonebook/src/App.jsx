cf10ecb9b498dc9e535fb7a8f0752bef7ab716c8
Ayush <ayushsachan49@gmail.com>
2024-06-01 13:15:49 +0530

    phonebook step 9 and 10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -34,9 +34,11 @@ const PersonForm = ({ onSubmit, nameValue, numberValue, onNameChange, onNumberCh
     </form>
   )
 }
-const Person = ({ person }) => {
+const Person = ({ person,handleDelete }) => {
   return (
-    <p>{person.name} {person.number}</p>
+    <p>{person.name} {person.number} 
+    <button onClick={handleDelete}>delete</button>
+    </p>
   )
 }
 
@@ -49,8 +51,8 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
     useEffect(() => {
-    personService.getAll().then(initialNote => {
-        setPersons(initialNote)
+    personService.getAll().then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
 
@@ -63,15 +65,26 @@ const App = () => {
       name: trimmedName,
       number: newNumber
     }
-    if (persons.find(person => person.name === trimmedName)) {
+    if (persons.find(person => person.name === trimmedName && person.number === newNumber)) {
       alert(`${trimmedName} is already added to phonebook`)
+    }else if(persons.find(person => person.name === trimmedName)){
+        if(window.confirm(`${trimmedName} is already added to phonebook, replace the old number with a new one?`)){
+         const person=persons.find(person=>person.name===trimmedName).id
+         personService.update(person,personObject)
+          .then(updatedPerson=>{
+            console.log(updatedPerson)
+            setPersons(persons.map(person=>person.id!==updatedPerson.id?person:updatedPerson))
+           // Here it is important to use person.id!==updatedPerson.id instead of person!==updatedPerson because person is an object and updatedPerson is also an object so they will never be equal. These two have different reference in memory so they will never be satifsying this: "==="
+          })
+        }
     }
     else {
       // setPersons(persons.concat(personObject))
      personService
       .create(personObject)
-      .then(returnedNote=>{
-        setPersons(persons.concat(returnedNote))
+      .then(returnedPersons=>{
+        console.log(returnedPersons)
+        setPersons(persons.concat(returnedPersons))
       
       })    
     }
@@ -96,7 +109,19 @@ const App = () => {
     console.log(event.target.value)
     setFilter(event.target.value)
   }
-
+  
+  const handleDelete = (id) => {
+    const deletedPerson =persons.find(person => person.id === id)
+    const result = window.confirm(`Delete ${deletedPerson.name} ?`)
+    if(result){
+      personService
+      .deletePerson(id)
+      .then(deletedPersons=>{
+        setPersons(persons.filter(person=>person.id!==deletedPerson.id))
+      }) // Here actually there is no need of using deletedPerson.id, I could just use id(which is sent as parameter to handleDelete).
+    }
+  }
+    
 
   // return statement
   return (
@@ -114,7 +139,8 @@ const App = () => {
       />
       <h2>Numbers</h2>
        {/* {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)} */}
-      {persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase().trim())).map(person =>  <Person key={person.name} person={person} />)}
+      {persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase().trim())).map(person =>  <Person key={person.name} person={person} handleDelete={()=>handleDelete(person.id)} />)}
+     { /* Here it is important to send function in handleDelete as a function not as a function call because if we send function call then it will be called immediately*/}
       
     </div>
   )
--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,19 +1,31 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const baseUrl='http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/persons';
 
-const getAll= ()=> {
-    const request=axios.get(baseUrl)
-    return request.then(response => response.data)
-}
+const getAll = () => {
+    const request = axios.get(baseUrl);
+    return request.then(response => response.data);
+};
 
-const create= (newObject)=>{
-    const request=axios.post(baseUrl,newObject)
-    return request.then(response=>response.data)
-}
+const create = (newObject) => {
+    const request = axios.post(baseUrl, newObject);
+    return request.then(response => response.data);
+};
 
+const deletePerson = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`);
+    return request.then(response => response.data);
+};
 
-export default{
+const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject);
+    return request.then(response => response.data);
+};
+
+// This is a more convinient way of defining objecs(only when object literals and their values have same name)
+export default {
     getAll,
-    create
-}
+    create,
+    deletePerson,
+    update
+};
