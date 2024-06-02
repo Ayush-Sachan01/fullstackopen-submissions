746c28b2dafdc4531ff2405eb8b988c54b3fee5c
Ayush <ayushsachan49@gmail.com>
2024-06-02 15:46:26 +0530

    Data for countries step 1

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,33 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState,useEffect } from 'react'
+import countryService from './services/countries'
+
+const CountryList= ({countries})=>
+  {
+    return(
+      <div>
+        {countries.map(country => <p key={country.name.common}>{country.name.common}</p>)}
+      </div>
+    )
+  }
+
+  const CountryDetails = ({country}) => {
+    return(
+      <div>
+         <h1>{country.name.common}</h1>
+          capital {country.capital} <br/>
+          area {country.area} <br /> <br />
+
+          <strong>languages:</strong> <br />
+           <ul>
+            {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
+           </ul>
+          
+          <img src={country.flags.png} alt={country.name.common} width="100"  />
+      </div>
+    )
+  }
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [countries, setCountries] = useState([])
+  const [value, setValue] = useState('')
+  const [filteredCountries, setFilteredCountries] = useState([])
+
+  useEffect(() => {
+    countryService
+      .getAll()
+      .then(response => {
+        setCountries(response)
+      })
+  },[])
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+    const filteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(event.target.value.toLowerCase()))
+    console.log(filteredCountries)
+    console.log(filteredCountries[0])
+    setFilteredCountries(filteredCountries)
+  }
+  const handleShow=()=>{
+    if(value===''){
+      return <p></p>
+    }
+    else if(filteredCountries.length<=10 && filteredCountries.length>1){
+      return <CountryList countries={filteredCountries} />
+
+    }
+    else if(filteredCountries.length===1){
+      return <CountryDetails country={filteredCountries[0]} />
+      // here filteredCountries[0] is an object while filteredCountries is and array containing this one object. If you look at the CountryDetails component, you will see that it expects an object as a prop. That's why we pass filteredCountries[0] to it.
+    }
+    else if(filteredCountries.length>10){
+      return <p>Too many matches, specify another filter</p>
+    }
+    else{
+      return <p>No matches</p>
+    }
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        find countries <input
+        value={value}
+        onChange={handleChange}
+        />      
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        {handleShow()}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
